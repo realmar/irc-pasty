da396569250ace63e567260f0f91b61a607e63cd
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-07-21 22:10:32 +0200

    Preview button changes its value, formated preview-area

--- static/js/controller.js
+++ static/js/controller.js
@@ -8,10 +8,12 @@ function run() {
       $("#preview-area").empty();
       $("#preview-area").append(markdown.toHTML($("#edit-area > textarea").val()));
       $(this).data("mode", "preview");
+      $(this).html("Edit");
     }else{
       $(this).data("mode", "edit");
       $("#preview-area").hide();
       $("#edit-area").show();
+      $(this).html("Preview");
     }
   });
 }
