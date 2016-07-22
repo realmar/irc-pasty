d33c54a53389dbe8b939f70afd1a9682c64b15d7
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-07-23 00:16:40 +0200

    Client does autosave

--- static/js/controller.js
+++ static/js/controller.js
@@ -5,7 +5,42 @@ function setLink(link) {
   $("#pasty-link").attr("href", link);
 }
 
+function sendData(url, autosave) {
+  if($("#input-area").val() == "" || $("#input-area").val() == undefined) {
+    return 0; // IMPLEMENT: error handling
+  }
+  if($("#post-title").val() == "" || $("#input-title").val() == undefined) {
+    return 0; // IMPLEMENT: error handling
+  }
+  var id = "None"
+  if($("#post-id").data("post-id") != "") {
+    id = $("#post-id").data("post-id");
+  }
+  $.ajax({
+    url: url + id,
+    method: 'POST',
+    dataType: 'text',
+    data: {'input' : $("#input-area").val(), 'title' : $("#post-title").val()}
+  })
+  .done(function(response) {
+    if(autosave) {
+      window.history.replaceState({}, "Pasty", window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/getautosave/' + response);
+    }else{
+      setLink(window.location.protocol + '//' + window.location.hostname + '/get/' + response);
+      $("#pasty-link").parent('div').show();
+    }
+    $("#post-id").data("post-id", response);
+  })
+  .fail(function(jqXHR, text_status) {
+    console.log(text_status);
+  });
+}
+
 function run() {
+  setInterval(function() {
+    sendData('/autosave/', true)
+  }, 1000 * 60); // every minute
+
   if($("#mode-control").data("initial-view-mode") == "show") {
     $("#edit-area").hide();
     var content = $("#preview-area").html()
@@ -42,28 +77,6 @@ function run() {
   });
 
   $("#save").click(function() {
-    if($("#input-areaa").val() == "") {
-      return 0; // IMPLEMENT: error handling
-    }
-    if($("#post-title").val() == "") {
-      return 0; // IMPLEMENT: error handling
-    }
-    var id = "None"
-    if($("#post-id").data("post-id") != "") {
-      id = $("#post-id").data("post-id");
-    }
-    $.ajax({
-      url: '/save/' + id,
-      method: 'POST',
-      dataType: 'text',
-      data: {'input' : $("#input-area").val(), 'title' : $("#post-title").val()}
-    })
-    .done(function(response) {
-      setLink(window.location.protocol + '//' + window.location.hostname + '/get/' + response);
-      $("#pasty-link").parent('div').show();
-    })
-    .fail(function(jqXHR, text_status) {
-      console.log(text_status);
-    });
+    sendData('/save/', false);
   });
 }
