958f1a298f9a5ac6b3e59513cd7cdf8102bad341
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-11-11 08:50:42 +0000

    Added unit tests for invalid filepath in delete, deleteFile, getAllPosts

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -2,9 +2,20 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from lib.config_checker import *
+from lib.poster import *
 
 def test_config():
     assert configCheck(None) == False
     assert configCheck({}) == False
     
     assert os.path.exists('pasty_server.conf')
+
+def test_invalid_deleteFile():
+    assert deleteFile('/some/noneexisting/file')
+
+def test_invalid_delete():
+    ret = delete('/some/noneexisting/directory', 'file', 'id')
+    assert ret or ret == None
+
+def test_invalid_getAllPosts():
+    assert getAllPosts('/some/noneexisting/directory')
