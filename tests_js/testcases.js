b62d54fcb39ebe48e68f1b582307e818dbe02128
Anastassios Martakos <anastassios.martakos@gmail.com>
2016-11-11 11:20:27 +0000

    Added mock template builder for js tests

--- tests_js/testcases.js
+++ tests_js/testcases.js
@@ -1,4 +1,24 @@
-QUnit.test( "a basic test example", function( assert ) {
-  var value = "hello";
-  assert.equal( value, "hello", "We expect value to be hello" );
+// initial setup
+
+// do not start automatically, we need first need to fetch the html
+QUnit.config.autostart = false;
+
+$.ajax({
+  url: '../templates.html',
+  dataType: 'html',
+  success: function(html) {
+      
+    // find specific elements you want...
+    var elem = $(html).find('body');
+    $('#qunit-fixture').append(elem);
+
+    QUnit.start();
+  }
+});
+
+// tests
+
+QUnit.test( "a basic test example", function(assert) {
+    var value = "hello";
+    assert.equal( value, "hello", "We expect value to be hello" );
 });
