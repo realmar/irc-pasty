54513d43d7b09d8943daef0f89d8ce33171c4f52
Claude Becker <becker@phys.ethz.ch>
2019-12-04 16:37:25 +0100

    explicit imports

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -2,9 +2,9 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from lib.config_checker import *
-from lib.poster import *
-from lib.tools import *
+from lib.config_checker import configCheck
+from lib.poster import delete, deleteFile, getAllPosts
+from lib.tools import sanitize_filename
 
 
 def test_config():
