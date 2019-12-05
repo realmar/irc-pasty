realmar/irc-pasty

commit 9e795fdadf3043acf591b329ae2b51c96a16c080

fix typo in variable name

lib/poster.py
@@ -27,7 +27,7 @@ def savePostTopLevel(
         remote_user=None):
     """Add and format options of post and delegate save to savePost function."""
     if datetime is not None:
-        dir = os.path.join(directory, buildDateURL(datetime))
+        directory = os.path.join(directory, buildDateURL(datetime))
 
     if datetime is None:
         datetime = dt.today()

commit 267ba50a5a09544b11ad25583381990ec1bbdb84

date is already appended in savePost
fixes a bug introduced with commit 9e795fda

lib/poster.py
@@ -26,9 +26,6 @@ def savePostTopLevel(
         title, content, display_mode, datetime, id, directory,
         remote_user=None):
     """Add and format options of post and delegate save to savePost function."""
-    if datetime is not None:
-        directory = os.path.join(directory, buildDateURL(datetime))
-
     if datetime is None:
         datetime = dt.today()
 